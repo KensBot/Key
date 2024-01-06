a8cb3637dd64382dcc0f70ad8db6922ee185aeaf
KensBot <kensbotz@gmail.com>
2024-01-07 02:56:54 +0700

    first commit

---
+++ controllers/message.js
@@ -0,0 +1,45 @@
+require("./settings")
+msg = {
+    paramkey: {
+        creator: creator,
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Apikey Parameter Tidak Ditemukan"
+    },
+    paramquery: {
+        creator: creator,
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Query Parameter Tidak Ditemukan"
+    },
+    paramtext: {
+        creator: creator,
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Text Parameter Tidak Ditemukan"
+    },
+    paramtext2: {
+        creator: creator,
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Text2 Parameter Tidak Ditemukan"
+    },
+    paramurl: {
+        creator: creator,
+    	status: "Terjadi Kesalahan",
+    	code: 403,
+    	message: "Url Parameter Tidak Ditemukan"
+    },
+    nodata: {
+        creator: creator,
+    	status: "Error",
+    	code: 404,
+    	message: "Error Data Tidak Di Temukan"
+    },
+    error: {
+        creator: creator,
+    	status: "Error",
+    	code: 404,
+    	message: "Error Tidak Di Temukan"
+    }
+}
---
+++ controllers/settings.js
@@ -0,0 +1,13 @@
+global.creator = "@nexon.js ─ Kens Ransyah";
+global.key_free = ["freekey"];
+global.key_premium = ['Zenith', 'Nexon'];
+global.limit_free = "200";
+global.limit_premium = "99999";
+global.dateLimit = "Oktober, 31 2022",
+global.mongo_Db = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "nexonnjs@gmail.com";
+global.my_email_password = "nghgxbikjiyjecdr";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 8080
