ec4ab6064eb79c697e875647af67a3b22f5eacb7
Kens <kensbotz@gmail.com>
2024-01-07 03:23:07 +0700

    Update index.js

--- scrape/index.js
+++ scrape/index.js
@@ -1,6 +1,6 @@
 exports.downloader = require('./src/downloader');
 exports.search = require('./src/search');
-exports.textpro = require('./src/textpro');
+//exports.textpro = require('./src/textpro');
 exports.photooxy = require('./src/photooxy');
 exports.welcome = require('./src/welcome');
 exports.others = require('./src/others');
