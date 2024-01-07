8c9ee68bb6d22548f9f7de2447930fc0e379bd45
Kens <kensbotz@gmail.com>
2024-01-07 11:03:40 +0700

    Update settings.js

--- controllers/settings.js
+++ controllers/settings.js
@@ -4,7 +4,7 @@ global.key_premium = ['Zenith', 'Nexon'];
 global.limit_free = "200";
 global.limit_premium = "99999";
 global.dateLimit = "Oktober, 31 2022",
-global.mongo_Db = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority";
+global.mongo_Db = "mongodb+srv://mochyukiw:anjay123@cluster0.we50obk.mongodb.net/?retryWrites=true&w=majority";
 global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
 global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
 global.my_email = "nexonnjs@gmail.com";
